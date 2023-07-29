4f7c81c6f0cd7b0714e07e116589fa7c61318aa5
ali <aliabbasi.95@yahoo.com>
2023-07-29 14:25:30 +0330

    use validators in serializers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,18 +1,17 @@
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
+import django.contrib.auth.password_validation as validators
 
 User = get_user_model()
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(
-                  validators=[UniqueValidator(queryset=User.objects.all())]
-    )
+    email = serializers.EmailField(validators=[UniqueValidator(queryset=User.objects.all())])
     username = serializers.CharField(
         max_length=32, validators=[UniqueValidator(queryset=User.objects.all())]
-                                     )
-    password = serializers.CharField()
+    )
+    password = serializers.CharField(validators=[validators.validate_password])
 
     class Meta:
         model = User
