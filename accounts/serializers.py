229e796f318f973787379d48ffa946a31921292b
ali <aliabbasi.95@yahoo.com>
2023-07-29 14:05:54 +0330

    add UniqueValidator for username and email in serializers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework.validators import UniqueValidator
 
 User = get_user_model()
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField()
-    username = serializers.CharField(max_length=32)
+    email = serializers.EmailField(
+                  validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
+        max_length=32, validators=[UniqueValidator(queryset=User.objects.all())]
+                                     )
     password = serializers.CharField()
 
     class Meta:
