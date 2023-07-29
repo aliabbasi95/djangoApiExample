227c28826a354dee243c793c3bcf2c207069e421
ali <aliabbasi.95@yahoo.com>
2023-07-29 14:29:55 +0330

    change register status code to 201

--- accounts/views.py
+++ accounts/views.py
@@ -13,5 +13,5 @@ class UserRegistrationView(APIView):
 
         if serializer.is_valid(raise_exception=True):
             serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_200_OK)
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
