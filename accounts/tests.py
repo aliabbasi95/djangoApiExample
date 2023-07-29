55079cb2891146512b7ee38ec86c37fef2c27fc5
ali <aliabbasi.95@yahoo.com>
2023-07-29 15:20:02 +0330

    add test_register_user_with_short_password

--- accounts/tests.py
+++ accounts/tests.py
@@ -32,3 +32,21 @@ class AccountsTest(APITestCase):
         self.assertEqual(response.data['username'], data['username'])
         self.assertEqual(response.data['email'], data['email'])
         self.assertFalse('password' in response.data)
+
+    def test_register_user_with_short_password(self):
+        """
+        Ensure user is not created for password lengths less than 12.
+        """
+        data = {
+            'username': 'foobar',
+            'email': 'foobarbaz@example.com',
+            'password': 'f@2F'
+        }
+
+        response = self.client.post(self.register_url, data, format='json')
+        # We want to make sure we have one user in the database.
+        self.assertEqual(User.objects.count(), 1)
+        # And that we're returning a 400 created code.
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # And check length of password in response data equal 1
+        self.assertEqual(len(response.data['password']), 1)
